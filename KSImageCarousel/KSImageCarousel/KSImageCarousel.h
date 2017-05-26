db8a6b580832314f000fda0021bc67a794b7524b
Lee Kah Seng <kahseng.lee123@gmail.com>
2017-05-27 07:48:47 +0800

    Initial commit

---
+++ KSImageCarousel/KSImageCarousel/KSImageCarousel.h
@@ -0,0 +1,19 @@
+//
+//  KSImageCarousel.h
+//  KSImageCarousel
+//
+//  Created by Lee Kah Seng on 24/05/2017.
+//  Copyright © 2017 Lee Kah Seng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSImageCarousel.
+FOUNDATION_EXPORT double KSImageCarouselVersionNumber;
+
+//! Project version string for KSImageCarousel.
+FOUNDATION_EXPORT const unsigned char KSImageCarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSImageCarousel/PublicHeader.h>
+
+
---
+++ KSImageCarousel/KSImageCarouselTests/KSImageCarouselTests.swift
@@ -0,0 +1,27 @@
+//
+//  KSImageCarouselTests.swift
+//  KSImageCarouselTests
+//
+//  Created by Lee Kah Seng on 24/05/2017.
+//  Copyright © 2017 Lee Kah Seng. All rights reserved.
+//
+
+import XCTest
+@testable import KSImageCarousel
+
+class KSImageCarouselTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+  
+    }
+}
